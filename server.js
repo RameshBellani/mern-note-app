59bbfd0e6a7a0e8239c56efd2d3939128e39614e
RameshBellani <136491880+RameshBellani@users.noreply.github.com>
2024-05-31 19:40:10 +0530

    updated env

--- server.js
+++ server.js
@@ -1,58 +1,57 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const authRouter = require('./routes/auth')
-const dataModelSchema = require('./models/dataModel')
-const verifyToken = require('./verifyToken')
+const authRouter = require('./routes/auth');
+const dataModelSchema = require('./models/dataModel');
+const verifyToken = require('./verifyToken');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
 const app = express();
 const port = 3004;
 
 app.use(cors());
 app.use(express.json());
-app.use('/auth', authRouter)
+app.use('/auth', authRouter);
 
-
-
-mongoose.connect(env.mongodb.URL, {
+mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 }).then(
-  () => console.log("db connetcted")
-  ).catch(err => console.log(err));
-
+  () => console.log('db connected')
+).catch(err => console.log(err));
 
 app.get('/', verifyToken, async (req, res) => {
-  if(req && req.decodedToken){
+  if (req && req.decodedToken) {
     try {
       const notes = await dataModelSchema.find({ user: req.decodedToken._id });
       res.json(notes);
-  } catch (error) {
+    } catch (error) {
       console.error(error);
       res.status(500).send('Internal Server Error');
+    }
   }
-  }
-    
 });
 
-
 app.post('/add', verifyToken, async (req, res) => {
-    if(req && req.decodedToken){
-      try {
-        const note = {
-          title : req.body.title,
-          description : req.body.description,
-          imageUrl: req.body.imageUrl,
-          url: req.body.url,
-          user: req.decodedToken._id
-        }
+  if (req && req.decodedToken) {
+    try {
+      const note = {
+        title: req.body.title,
+        description: req.body.description,
+        imageUrl: req.body.imageUrl,
+        url: req.body.url,
+        user: req.decodedToken._id,
+      };
 
-        const Note = await dataModelSchema.create(note)
-        res.status(201).json(Note);
+      const Note = await dataModelSchema.create(note);
+      res.status(201).json(Note);
     } catch (error) {
-        console.error(error);
-        res.status(500).send('Internal Server Error');
+      console.error(error);
+      res.status(500).send('Internal Server Error');
     }
-    }
+  }
 });
 
 app.put('/edit/:id', async (req, res) => {
@@ -67,7 +66,6 @@ app.delete('/delete/:id', async (req, res) => {
   await dataModelSchema.findByIdAndDelete(id);
   res.json({ message: 'Note deleted' });
 });
-
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
